f1c0fd6e9cb56652ab5cba39a5c9e52cd932b446
Prasanna Venkatesh Raghunathan <70012245+PrasannaVenkatesh08@users.noreply.github.com>
2021-03-06 09:20:54 +0530

    Update API_URL.tsx

--- src/components/Common/API_URL.tsx
+++ src/components/Common/API_URL.tsx
@@ -1,5 +1,5 @@
 export default class API_URL{
-    static article_Url : string = "/svc/search/v2/articlesearch.json?api-key=c22TZa07KBtaA2udYVxCQviDGxqgxsnc" ;
-    static story_Url : string = "/svc/topstories/v2/" ;
+    static article_Url : string = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=c22TZa07KBtaA2udYVxCQviDGxqgxsnc" ;
+    static story_Url : string = "https://api.nytimes.com/svc/topstories/v2/" ;
     static apiKEy : string = ".json?api-key=c22TZa07KBtaA2udYVxCQviDGxqgxsnc"
 }
